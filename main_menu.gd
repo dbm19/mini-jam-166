bc14e5faaec3ce81fea9561a5323f7b883745d08
Daniel Mau <daniel.b.mau@gmail.com>
2024-08-31 16:22:42 -0400

    Added effects to main menu buttons

--- main_menu.gd
+++ main_menu.gd
@@ -1,12 +1,13 @@
 extends Control
 
 var new_game_button
+var tutorial_button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game_button = get_node("VBoxContainer/NewGameButton")
-
+	new_game_button = get_node("VBoxContainer2/NewGameButton")
+	tutorial_button = get_node("VBoxContainer2/TutorialButton")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,7 +16,6 @@ func _process(delta: float) -> void:
 func _on_new_game_button_button_down() -> void:
 	new_game_button.icon = load("res://art_assets/new_game_button_pressed.png")
 
-
 func _on_new_game_button_button_up() -> void:
 	new_game_button.icon = load("res://art_assets/new_game_button.png")
 
@@ -23,5 +23,20 @@ func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 	Global.ammo = 5 
 
+func _on_new_game_button_mouse_entered() -> void:
+	new_game_button.icon = load("res://art_assets/new_game_button_hovered.png")
+
+func _on_new_game_button_mouse_exited() -> void:
+	new_game_button.icon = load("res://art_assets/new_game_button.png")
+
 func _on_tutorial_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://tutorial_screen.tscn")
+
+func _on_tutorial_button_button_down() -> void:
+	tutorial_button.icon = load("res://art_assets/tutorial_button_pressed.png")
+
+func _on_tutorial_button_mouse_entered() -> void:
+	tutorial_button.icon = load("res://art_assets/tutorial_button_hovered.png")
+
+func _on_tutorial_button_mouse_exited() -> void:
+	tutorial_button.icon = load("res://art_assets/tutorial_button.png")
