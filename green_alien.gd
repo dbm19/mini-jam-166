973672717143b52449ac98ff1aff63cf184bc51b
Daniel Mau <daniel.b.mau@gmail.com>
2024-08-31 10:04:29 -0400

    fixed bugs

--- green_alien.gd
+++ green_alien.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	global_position += direction * speed * delta
 
-func _on_green_alien_area_area_entered(area: Area2D) -> void:
+func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
 		green_alien_sprite.texture = load("res://blue_alien.png")
 		area.queue_free()
